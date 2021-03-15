650b9b4bf334c25229bf6fb3f24dbfc45f2f8059
Brandon <braondon@bharvest.io>
2021-03-15 22:03:21 +0900

    feat: generalizing init script - adding comments for each steps - adding handler to stop if process is already running - using hidden data folder to bootstrap two chains - opening RPC, GRPC, REST ports for better testing environments - updating .gitignore

--- network/init.sh
+++ network/init.sh
@@ -1,31 +1,89 @@
 #!/bin/bash
-DEV_MNEMONIC_1="alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart"
-DEV_MNEMONIC_2="record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset"
 
-### Chain 1
-yes $DEV_MNEMONIC_1 | icad keys add val --recover --home ~/.demo-test-1
+# Configure predetermined settings
+# val1: cosmos1mjk79fjjgpplak5wq838w0yd982gzkyfrk07am
+# val2: cosmos17dtl0mjt3t77kpuhg2edqzjpszulwhgzuj9ljs
+BINARY=icad
+CHAINDIR=./data
+CHAINID1=test-1
+CHAINID2=test-2
+MNEMONIC1="alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart"
+MNEMONIC2="record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset"
+P2PPORT1=16656
+P2PPORT2=26656
+RPCPORT1=16657
+RPCPORT2=26657
+GRPCPORT1=8090
+GRPCPORT2=9090
+RESTPORT1=1316
+RESTPORT2=1317
 
-icad init --chain-id test-1 test-1 --home ~/.demo-test-1
+# Stop if it is already running 
+if pgrep -x "$BINARY" >/dev/null; then
+    echo "Terminating $BINARY..."
+    killall $BINARY
+fi
 
-sed -i -e 's/2665/1665/g' ~/.demo-test-1/config/config.toml
-sed -i -e 's#localhost:6060#localhost:6061#g' ~/.demo-test-1/config/config.toml
-sed -i -e 's#address = "0.0.0.0:9090"#address = "0.0.0.0:9092"#g' ~/.demo-test-1/config/app.toml
+# Remove previous data
+echo "Removing previous data..."
+rm -rf $CHAINDIR/$CHAINID1 &> /dev/null
+rm -rf $CHAINDIR/$CHAINID2 &> /dev/null
 
-icad add-genesis-account $(icad keys show val -a --home ~/.demo-test-1) 100000000000stake --home ~/.demo-test-1
-icad gentx val 7000000000stake --chain-id test-1 --home ~/.demo-test-1
+# Add directories for both chains, exit if an error occurs
+if ! mkdir -p $CHAINDIR/$CHAINID1 2>/dev/null; then
+    echo "Failed to create chain folder. Aborting..."
+    exit 1
+fi
 
-icad collect-gentxs --home ~/.demo-test-1
+if ! mkdir -p $CHAINDIR/$CHAINID2 2>/dev/null; then
+    echo "Failed to create chain folder. Aborting..."
+    exit 1
+fi
 
-### Chain 2
-yes $DEV_MNEMONIC_2 | icad keys add val2 --recover --home ~/.demo-test-2
+echo "Initializing $CHAINID1..."
+echo "Initializing $CHAINID2..."
+$BINARY init test --home $CHAINDIR/$CHAINID1 --chain-id=$CHAINID1
+$BINARY init test --home $CHAINDIR/$CHAINID2 --chain-id=$CHAINID2
 
-icad init --chain-id test-2 test-2 --home ~/.demo-test-2
+echo "Adding genesis accounts..."
+echo $MNEMONIC1 | $BINARY keys add val1 --home $CHAINDIR/$CHAINID1 --recover --keyring-backend=test 
+echo $MNEMONIC2 | $BINARY keys add val2 --home $CHAINDIR/$CHAINID2 --recover --keyring-backend=test 
+$BINARY add-genesis-account $($BINARY --home $CHAINDIR/$CHAINID1 keys show val1 --keyring-backend test -a) 100000000000stake  --home $CHAINDIR/$CHAINID1
+$BINARY add-genesis-account $($BINARY --home $CHAINDIR/$CHAINID2 keys show val2 --keyring-backend test -a) 100000000000stake  --home $CHAINDIR/$CHAINID2
 
-icad add-genesis-account $(icad keys show val2 -a --home ~/.demo-test-2) 100000000000stake --home ~/.demo-test-2
-icad gentx val2 7000000000stake --chain-id test-2 --home ~/.demo-test-2
+echo "Creating and collecting gentx..."
+$BINARY gentx val1 7000000000stake --home $CHAINDIR/$CHAINID1 --chain-id $CHAINID1 --keyring-backend test
+$BINARY gentx val2 7000000000stake --home $CHAINDIR/$CHAINID2 --chain-id $CHAINID2 --keyring-backend test
+$BINARY collect-gentxs --home $CHAINDIR/$CHAINID1
+$BINARY collect-gentxs --home $CHAINDIR/$CHAINID2
 
-icad collect-gentxs --home ~/.demo-test-2
+echo "Changing defaults and ports in app.toml and config.toml files..."
+sed -i '' 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:'"$P2PPORT1"'"#g' $CHAINDIR/$CHAINID1/config/config.toml
+sed -i '' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:'"$RPCPORT1"'"#g' $CHAINDIR/$CHAINID1/config/config.toml
+sed -i '' 's/timeout_commit = "5s"/timeout_commit = "1s"/g' $CHAINDIR/$CHAINID1/config/config.toml
+sed -i '' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' $CHAINDIR/$CHAINID1/config/config.toml
+sed -i '' 's/index_all_keys = false/index_all_keys = true/g' $CHAINDIR/$CHAINID1/config/config.toml
+sed -i '' 's/enable = false/enable = true/g' $CHAINDIR/$CHAINID1/config/app.toml
+sed -i '' 's/swagger = false/swagger = true/g' $CHAINDIR/$CHAINID1/config/app.toml
+sed -i '' 's#"tcp://0.0.0.0:1317"#"tcp://0.0.0.0:'"$RESTPORT1"'"#g' $CHAINDIR/$CHAINID1/config/app.toml
 
-#echo "Running both chains"
-icad start --pruning nothing --home ~/.demo-test-1 &
-icad start --pruning nothing --home ~/.demo-test-2 &
+sed -i '' 's#"tcp://0.0.0.0:26656"#"tcp://0.0.0.0:'"$P2PPORT2"'"#g' $CHAINDIR/$CHAINID2/config/config.toml
+sed -i '' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:'"$RPCPORT2"'"#g' $CHAINDIR/$CHAINID2/config/config.toml
+sed -i '' 's/timeout_commit = "5s"/timeout_commit = "1s"/g' $CHAINDIR/$CHAINID2/config/config.toml
+sed -i '' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' $CHAINDIR/$CHAINID2/config/config.toml
+sed -i '' 's/index_all_keys = false/index_all_keys = true/g' $CHAINDIR/$CHAINID2/config/config.toml
+sed -i '' 's/enable = false/enable = true/g' $CHAINDIR/$CHAINID2/config/app.toml
+sed -i '' 's/swagger = false/swagger = true/g' $CHAINDIR/$CHAINID2/config/app.toml
+sed -i '' 's#"tcp://0.0.0.0:1317"#"tcp://0.0.0.0:'"$RESTPORT2"'"#g' $CHAINDIR/$CHAINID2/config/app.toml
+
+# Start chain 1
+echo "Starting $CHAINID1 in $CHAINDIR..."
+echo "Creating log file at $CHAINDIR/$CHAINID1.log"
+$BINARY start --home $CHAINDIR/$CHAINID1 --pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT1" > $CHAINDIR/$CHAINID1.log 2>&1 &
+
+# Start chain 2
+echo "Starting $CHAINID1 in $CHAINDIR..."
+echo "Creating log file at $CHAINDIR/$CHAINID1.log"
+$BINARY start --home $CHAINDIR/$CHAINID2 --pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT2" > $CHAINDIR/$CHAINID2.log 2>&1 &
+
+
