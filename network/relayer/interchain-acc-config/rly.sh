f47a8fb782b2a47c263d061ea2b9e84b5fb7902e
Brandon <braondon@bharvest.io>
2021-03-15 22:04:42 +0900

    feat: generalizing rly script - adding comments for each step - using hidden data/relayer folder to link two chains

--- network/relayer/interchain-acc-config/rly.sh
+++ network/relayer/interchain-acc-config/rly.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
-DEV_MNEMONIC_1="alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart"
-DEV_MNEMONIC_2="record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset"
 
-rly config init
+# Configure predefined mnemonic pharses
+BINARY=rly
+CHAINDIR=./data
+RELAYERDIR=./relayer
+MNEMONIC1="alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart"
+MNEMONIC2="record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset"
 
-rly config add-chains $PWD/network/relayer/interchain-acc-config/chains
-rly config add-paths $PWD/network/relayer/interchain-acc-config/paths
+# Ensure rly is installed
+if ! [ -x "$(command -v $BINARY)" ]; then
+    echo "$BINARY is required to run this script..."
+    echo "You can download at https://github.com/cosmos/relayer"
+    exit 1
+fi
 
-rly keys restore test-1 test-1 "$DEV_MNEMONIC_1"
+echo "Initializing $BINARY..."
+$BINARY config init --home $CHAINDIR/$RELAYERDIR
 
-rly keys restore test-2 test-2 "$DEV_MNEMONIC_2"
+echo "Adding configurations for both chains..."
+$BINARY config add-chains $PWD/network/relayer/interchain-acc-config/chains --home $CHAINDIR/$RELAYERDIR
+$BINARY config add-paths $PWD/network/relayer/interchain-acc-config/paths --home $CHAINDIR/$RELAYERDIR
 
-rly light init test-1 -f
-rly light init test-2 -f
+echo "Restoring accounts..."
+$BINARY keys restore test-1 test-1 "$MNEMONIC1" --home $CHAINDIR/$RELAYERDIR
+$BINARY keys restore test-2 test-2 "$MNEMONIC2" --home $CHAINDIR/$RELAYERDIR
 
-rly tx path test1-account-test2
+echo "Initializing light clients for both chains..."
+$BINARY light init test-1 -f --home $CHAINDIR/$RELAYERDIR
+$BINARY light init test-2 -f --home $CHAINDIR/$RELAYERDIR
 
-rly start test1-account-test2
+echo "Linking both chains..."
+$BINARY tx path test1-account-test2 --home $CHAINDIR/$RELAYERDIR
+
+echo "Starting to listen relayer..."
+$BINARY start test1-account-test2 --home $CHAINDIR/$RELAYERDIR
